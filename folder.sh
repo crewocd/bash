13084ef7dcd985a9091c77cf51f04a9942f22919
devcusn <halilibrahimtunc00@gmail.com>
2022-08-17 19:32:03 +0300

    ls cd add

---
+++ folder.sh
@@ -0,0 +1,5 @@
+ls # The ls (list) command is used to list directories or files
+ls -a: # List all the files in the current directory including hidden files too
+ls -l: #Long listing of all the files and their size in the current directory
+
+cd #The cd (change directory) command is used to navigate in the directory tree structure.
